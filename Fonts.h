bfafd3301d83857a108646a7f2a65de23c5324da
Daniel <danielbr1999@o2.pl>
2020-05-24 14:59:46 +0200

    table of pointers

--- Fonts.h
+++ Fonts.h
@@ -7,7 +7,9 @@ using namespace std;
 class Fonts{
 public:
 	void createFont6x8();
+	void createFont7x10();
 private:
-
+	Letter** Font_6x8;
+	Letter** Font_7x10;
 };
 
--- Letter.cpp
+++ Letter.cpp
@@ -5,5 +5,5 @@ Letter::Letter(uint16_t* tab, uint8_t height, uint8_t width) {
 	this->width = width;
 	this->letter = new uint16_t[height];
 	for (uint8_t i = 0; i < height; i++)
-		this->letter[i] = tab[i] << MAX_WIDTH - width;
+		this->letter[i] = tab[i] << (MAX_WIDTH - width);
 }
